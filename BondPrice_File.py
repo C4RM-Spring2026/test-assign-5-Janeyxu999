2a7155337a3259a9ed458a4f6fae5e17c0633370
Janeyxu999 <Janeyxu999@163.com>
2026-02-28 23:47:52 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -2,12 +2,13 @@ import numpy as np
 
 def getBondPrice(y, face, couponRate, m, ppy=1):
     m_eff = m * ppy
-    couponRate_eff = couponRate / ppy
     y_eff = y / ppy
+    couponRate_eff = face * couponRate / ppy
 
     t = np.arange(1, m_eff + 1)
+
     discount = 1 / (1 + y_eff) ** t
 
-    price = np.sum(face * couponRate_eff * discount) + face * discount[-1]
+    price = np.sum(couponRate_eff * discount) + face * discount[-1]
 
     return price
